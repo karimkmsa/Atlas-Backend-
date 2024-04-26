01ad99697ad7a5bb5875ef8c4d6e374214deebcd
karim khaled <khaledkarim808@gmail.com>
2024-04-27 02:05:45 +0300

    globalErr

--- src/middleware/globalErrorMiddleware.js
+++ src/middleware/globalErrorMiddleware.js
@@ -1,8 +1,7 @@
-export const globalError= (err,req,res,next)=>{
-    console.log(err)
-    process.env.MODE == "devlopment"? 
-    res.status(err.statusCode).json({err:err.message,stack:err.stack})
-    : res.status(err.statusCode).json({err:err.message})
-
-   
-}     
+export const globalError = (err, req, res, next) => {
+    let error = err.message;
+    let code = err.statuscode || 500;
+    process.env.MODE == "dev"
+      ? res.status(code).json({ error, stack: err.stack })
+      : res.status(code).json({ error });
+  };
