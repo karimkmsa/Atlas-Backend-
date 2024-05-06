05e0233544b02ea656a4aee625dd348df8102b0c
karim khaled <khaledkarim808@gmail.com>
2024-05-06 20:32:42 +0300

    auth update

--- src/modules/grades/grades.routes.js
+++ src/modules/grades/grades.routes.js
@@ -4,10 +4,10 @@ import { checkRole, verifyToken } from '../../middleware/authToken.js';
 
 const router = express.Router()
 
-router.post("/add-grade",verifyToken,checkRole('teacher'||'admin'),AddGrade)
+router.post("/add-grade",verifyToken,checkRole(['teacher','admin']),AddGrade)
 router.get("/getAllGrades",getAllGrades)
-router.put("/update-grade",verifyToken,checkRole('teacher'||'admin'),updateGrade)
-router.delete("/delete-grade",verifyToken,checkRole('teacher'||'admin'),deleteGrade)
+router.put("/update-grade",verifyToken,checkRole(['teacher','admin']),updateGrade)
+router.delete("/delete-grade",verifyToken,checkRole(['teacher','admin']),deleteGrade)
 
 
 export default router
